1ffc0d378bc1889b4564908ec60cb916de3d2516
hoangton <smartcash.cc@gmail.com>
2019-05-31 02:12:54 +0700

    Update BC-enable-tor-node.sh
    fix permisions

--- BC-enable-tor-node.sh
+++ BC-enable-tor-node.sh
@@ -10,11 +10,11 @@ cd
 sudo apt install -y build-essential libssl-dev git
 sudo git clone https://github.com/ReclaimYourPrivacy/eschalot.git
 cd eschalot
-make
-./eschalot -vp bc -t 1 > bconion
-sed '1d' bconion > bconion-temp
-sed -n 1p bconion-temp >hostname
-sed '1d' bconion-temp > private_key
+sudo make
+sudo ./eschalot -vp bc -t 1 > bconion
+sudo sed '1d' bconion > bconion-temp
+sudo sed -n 1p bconion-temp >hostname
+sudo sed '1d' bconion-temp > private_key
 
 # Step 3 Create folder to store hostname and private-key for tor
 echo 'Step 3 Create folder to store hostname and private-key for tor'
@@ -29,6 +29,7 @@ echo 'Step 4 append 2 line to Tor config'
 echo "HiddenServiceDir /var/lib/tor/bitcoinc-service/" | sudo tee -a /etc/tor/torrc > /dev/null
 echo "HiddenServicePort 9789 127.0.0.1:9789" | sudo tee -a /etc/tor/torrc > /dev/null
 
+sudo service tor restart
 
 #Step 5 Update binconc.conf and restart wallet 
 echo 'Step 5 Update binconc.conf and restart wallet '
@@ -41,7 +42,9 @@ echo 'addnode=ibt4q3cri3hs47f2.onion' >> ~/.bitcoinc/bitcoinc.conf
 echo -n "externalip=" | cat - /var/lib/tor/bitcoinc-service/hostname >> ~/.bitcoinc/bitcoinc.conf
 
 # Stop wallet 
+cd
 ./bitcoinc-cli stop && sleep 5
 
 # Start wallet
+cd
 ./bitcoincd && sleep 5 && ./bitcoinc-cli walletsettings stakingstatus true
