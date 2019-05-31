9806989843528fc746459ef7c53613c19c19e7d7
hoangton <smartcash.cc@gmail.com>
2019-05-31 14:53:15 +0700

    Update BC-enable-tor-node.sh
    remove old config when run 2nd time

--- BC-enable-tor-node.sh
+++ BC-enable-tor-node.sh
@@ -20,17 +20,21 @@ sed '1d' bconion-temp > private_key
 echo 'Step 3 Create folder to store hostname and private-key for tor'
 mkdir /var/lib/tor/bitcoinc-service
 chown -R debian-tor.debian-tor /var/lib/tor/bitcoinc-service
+chmod -R 700 /var/lib/tor/bitcoinc-service
 cp hostname /var/lib/tor/bitcoinc-service
 cp private_key /var/lib/tor/bitcoinc-service
 
 
 # Step 4 append 2 line to Tor config
 echo 'Step 4 append 2 line to Tor config'
+sed -i '/bitcoinc-service/d' /etc/tor/torrc
+sed -i '/9789/d' /etc/tor/torrc
 echo "HiddenServiceDir /var/lib/tor/bitcoinc-service/" | sudo tee -a /etc/tor/torrc > /dev/null
 echo "HiddenServicePort 9789 127.0.0.1:9789" | sudo tee -a /etc/tor/torrc > /dev/null
 
 # Step 5 Restart Tor
 echo 'Step 5 Restart Tor'
+sed -i '/bitcoinc-service/d' /etc/apparmor.d/system_tor
 sed -i '6 a    owner /var/lib/tor/bitcoinc-service/** rwk,' /etc/apparmor.d/system_tor
 /etc/init.d/apparmor restart
 /etc/init.d/tor restart
